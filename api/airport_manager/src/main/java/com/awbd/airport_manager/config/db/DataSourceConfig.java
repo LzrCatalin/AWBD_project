eaabe0118833d54bddd015a3780100f4131baf0d
Catalin Lazar <catalin.lazar@yotoka.com>
2026-06-03 21:26:00 +0300

    bug fix

--- api/airport_manager/src/main/java/com/awbd/airport_manager/config/db/DataSourceConfig.java
+++ api/airport_manager/src/main/java/com/awbd/airport_manager/config/db/DataSourceConfig.java
@@ -1,8 +1,5 @@
 package com.awbd.airport_manager.config.db;
 
-import com.zaxxer.hikari.HikariDataSource;
-import liquibase.integration.spring.SpringLiquibase;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jpa.autoconfigure.JpaProperties;
 import org.springframework.context.annotation.Bean;
@@ -13,24 +10,9 @@ import org.springframework.context.annotation.Primary;
 public class DataSourceConfig {
 
     @Primary
-    @Bean(name = "projectDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.airport-manager")
-    public HikariDataSource dataSource() {
-        return new HikariDataSource();
-    }
-
-    @Primary
     @Bean(name = "projectJpaProperties")
     @ConfigurationProperties(prefix = "spring.jpa.airport-manager")
     public JpaProperties jpaProperties() {
         return new JpaProperties();
-    }
-
-    @Bean
-    public SpringLiquibase projectLiquibase(@Qualifier("projectDataSource") HikariDataSource projectDataSource) {
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDataSource(projectDataSource);
-        liquibase.setChangeLog("classpath:/liquibase/changelog.xml");
-        return liquibase;
     }
 }
