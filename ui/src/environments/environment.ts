d993c07a3a1533a04734cc1350787a5cbda07472
Catalin Lazar <catalin.lazar@yotoka.com>
2026-06-10 23:25:45 +0300

    hidden secrets

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
   auth0: {
-    domain: 'dev-fz464d04imnu8jll.eu.auth0.com',
-    clientId: 'SCQriHuGoUkZEWr4uhHu7ixSDxxJK4Wt',
+    domain: 'dev-***.eu.auth0.com',
+    clientId: '***',
     redirectUri: window.location.origin,
     audience: 'https://airport-manager-api',
   },
